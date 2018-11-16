ce0e8946bb99f25f27190af626cd679c6a8e3b36
EC2 Default User <ec2-user@ip-172-31-28-67.ap-southeast-1.compute.internal>
2018-11-16 08:54:23 +0000

    make groups

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,6 +42,11 @@ class User < ActiveRecord::Base
    "#{family_name_kana} #{first_name_kana}"
   end
   
+  def full_profile?
+    image.present? && family_name? && first_name? && family_name_kana? && first_name_kana?
+  end
+  
+  
   private
   def has_group_key?
     group_key.present?
